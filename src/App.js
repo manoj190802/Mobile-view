0eb15998419753dea57b1f36ea8522f62a20356c
manoj190802 <manoj.manojlenin@gmail.com>
2026-03-07 13:27:44 +0530

    component7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './component/Home/Home'
+import About from './component/About/About'
+import Service from './component/Service/Service'
+import Contact from './component/Contact/Contact'
+import Login from './component/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+       <Navbar/>
+       <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route path="/about" element={<About Us/>}/>
+        <Route path="/service" element={<Service/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+       </Routes>
+      </BrowserRouter>
+   
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Service</li>
+        <li>Contact</li>
+        <button>Login</button>
+     </ul>
+    </div>
+  )
+}
