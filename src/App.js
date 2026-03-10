3050876155957520f155b5af46956b25e78afe41
manoj190802 <manoj.manojlenin@gmail.com>
2026-03-10 11:23:42 +0530

    component css9

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Customer Management';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './component/Home/Home'
-import About from './component/About/About'
-import Service from './component/Management/Sales Pipeline'
-import Contact from './component/Lead Management/Lead Management'
-import Login from './component/Login/Login'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Home from './component/Home/Home'
+// import About from './component/About/About'
+// import Service from './component/Management/Sales Pipeline'
+// import Contact from './component/Lead Management/Lead Management'
+// import Login from './component/Login/Login'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
        <Navbar/>
-       <Routes>
+       {/* <Routes>
         <Route  path="/" element={<Dashboard/>}/>
         <Route path="/about" element={<About Us/>}/>
         <Route path="/service" element={<Service/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
        </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
    
     </div>
   );
